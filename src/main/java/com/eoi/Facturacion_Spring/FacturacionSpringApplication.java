95fa0aa1feff56795146e367a2d949244fd28929
Karim Rachidi <karim.m.rachidi@gmail.com>
2023-04-11 22:26:39 +0200

    Initial commit

---
+++ src/main/java/com/eoi/Facturacion_Spring/FacturacionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.eoi.Facturacion_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eoi/Facturacion_Spring/FacturacionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eoi.Facturacion_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
